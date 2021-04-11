b7cfa05df8951fb28580df9f89429f141476b12e
Yuriko Takahashi <yuricocoa3618@gmail.com>
2021-04-11 21:36:36 +0900

    4章 機械語

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,59 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// 初期化
+  @color
+  M=0   // color=0
+  @8192
+  D=A
+  @pixels
+  M=D   // pixels=8192
+
+// 無限ループ
+(LOOP)
+  @i
+  M=1  // i=1
+  @KBD
+  D=M   // D=0or1(キーボード入力)
+  @BLACK
+  D;JNE // もしD!=0なら、キーボードが押されているのでBLACKへ移動
+  @WHITE
+  D;JEQ // もしD=0なら、キーボードが押されていないのでWHITEへ移動
+
+// 画面を一色に描画する
+(PAINT)
+  @i
+  D=M   // D=i
+  @pixels
+  D=D-M // D=i-pixels
+  @LOOP
+  D;JGT // もし(i-pixels)>0ならLOOPへ移動
+  @SCREEN
+  D=A-1 // D=16383(i=1スタートなので)
+  @i
+  D=D+M // D=16383+i
+  @position
+  M=D   // position=16383+i
+  @color
+  D=M   // D=color
+  @position
+  A=M   // @position
+  M=D   // 白or黒にペイント
+  @i
+  M=M+1 // i=i+1
+  @PAINT
+  0;JMP // PAINTへ移動
+
+// color=1にする
+(BLACK)
+  @color
+  M=-1   // color=111111111111111
+  @PAINT
+  0;JMP // PAINTへ移動
+
+// color＝0にする
+(WHITE)
+  @color
+  M=0   // color=0000000000000000
+  @PAINT
+  0;JMP // PAINTへ移動
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,27 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// R0*R1の結果をR2に格納する
+// sumにR1の値をR0回加える
+  @i
+  M=1   // i=1
+  @R2
+  M=0   // R2=0
+(LOOP)
+  @i
+  D=M   // D=i
+  @R0
+  D=D-M // D=i-R0
+  @END
+  D;JGT // もし(i-R0)>0ならENDへ移動
+  @R1
+  D=M   // D=R1
+  @R2
+  M=D+M // R2=R2+R1
+  @i
+  M=M+1 // i=i+1
+  @LOOP
+  0;JMP // LOOPへ移動
+(END)
+  @END
+  0;JMP
